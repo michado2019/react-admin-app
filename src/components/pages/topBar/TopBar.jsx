e89b7c7c29b345558a9fe85e565c04d3b4b8fab1
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-09-01 10:16:28 +0100

    topbar modified for font

--- src/components/pages/topBar/TopBar.jsx
+++ src/components/pages/topBar/TopBar.jsx
@@ -4,28 +4,19 @@ import { MenuOutlined } from '@mui/icons-material'
 
 export default function TopBar() {
 
-// const cancel = document.getElementById("cancel")
-
-// menu.addEventListener("click", displayMenu)
-// cancel.addEventListener("click", clearMenu)
-
 function handleDisplayMenuContent(){
     document.querySelector('.sideBar-wrapper').style.display = "block"
     document.querySelector('.topBar-menu_icon').style.display = "none"
 
 }
 
-// function clearMenu(){
-//     document.getElementById("nav--links").style.display = "none"
-   
-// }
   return (
     <div className='topBar-wrapper'>
       <div className='topBar-top'>
         <div className='topBar-top_welcome'>
           <MenuOutlined className='topBar-menu_icon' onClick={handleDisplayMenuContent}/>
           <h1 className='topBar-top_name'>Hi, Sam Afolabi, </h1>
-          <p className='topBar-top_greetings'>Welcome to<span className='topBar-top_app'>MY TAXI DASHBOARD</span></p>
+          <p className='topBar-top_greetings'>Welcome to<span className='topBar-top_app'>my Admin Dashboard</span></p>
         </div>
         <div className='topBar-search_div'>
           <input type='search' placeholder='search....' className='topBar-search' />
